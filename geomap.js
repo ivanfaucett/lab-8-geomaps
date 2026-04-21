51d5d36903cec3ba401b0a02576aa0ba6e9feb8d
Ivan <tup46067@temple.edu>
2026-04-21 16:10:13 -0400

    Activity 1

--- geomap.js
+++ geomap.js
@@ -3,8 +3,29 @@
     var width = 800,
         height = 600;
 
-    const svg = d3.select("#geomap")
-        .append("g")
-        .attr("transform", "translate(50,50)");
+    d3.json("./topo.json").then((data) => {
+
+        const topo = topojson.feature(data, data.objects.states);
+
+        var projection = d3.geoAlbersUsa()
+            .scale(700)
+            .translate([487.5, 305]);
+
+        var path = d3.geoPath(projection);
+
+        const svg = d3.select("#geomap")
+            .append("g")
+            .attr("transform", "translate(50,50)");
+
+        svg.append("g")
+            .selectAll("path")
+            .data(topo.features)
+            .join("path")
+            .attr("d", path)
+            .attr("fill", "whitesmoke")
+            .attr("stroke", "black")
+            .attr("stroke-width", "1px");
+
+    });
 
 })();
